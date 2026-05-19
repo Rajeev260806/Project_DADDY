38577262505add81ca344a92a1b20d8125360fc0
Rajeev Ganesan <rajeevganesan26@gmail.com>
2026-05-19 11:04:42 +0530

    Created memory.py and created a class to manage the history of the chat

---
+++ core/memory.py
@@ -0,0 +1,25 @@
+from config import MAX_HISTORY
+
+class ConversationHistory:
+
+    def __init__(self):
+        self.history = []
+
+    def add_user_message(self,message):
+        self.history.append({"role":"user","content":message})
+        self.trimHistory()
+
+    def add_agent_message(self,message):
+        self.history.append({"role":"agent","content":message})
+        self.trimHistory()
+    
+    def getHistory(self):
+        return self.history.copy()
+    
+    def trimHistory(self):
+        if len(self.history)>MAX_HISTORY*2:
+            self.history = self.history[-(MAX_HISTORY*2):]
+    
+    def clearMemory(self):
+        self.history = []
+        print("Memory Cleared!")
